28000ce8b259c95d86d1ffd1b0d93d79844c207e
KinchDev <kinich.development@gmail.com>
2023-07-09 21:50:15 -0600

    commit

--- controlador/controlador.js
+++ controlador/controlador.js
@@ -16,7 +16,8 @@ function DOM(){
             
             let informacion_cartas_porte = mkObj(pestaña_inicio,"informacion_cartas_porte","informacion_cartas_porte")
                 let img_sat = mkObjImg(informacion_cartas_porte,"img_sat","img_sat","./vista/img/logo_sat.png")
-                let inf_carta_porte = mkObj(informacion_cartas_porte,"introduccion_inicio","introduccion_inicio_info","<p>La información necesaria para la elaboración de cartas porte incluye <span>NOMBRE</span> y <span>DIRECCIÓN</span> del Remitente</p>")
+                let inf_carta_porte = mkObj(informacion_cartas_porte,"introduccion_inicio","introduccion_inicio_info","<p>Información relacionada con facturación: <br>-- Nombre de quien ofrece el servicio de transporte/flete</p>")
+
             
 
 }
